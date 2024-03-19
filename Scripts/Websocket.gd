3e9a973a83d84ab5a0cc56b2a384e530eb60e791
ell <connellsensational@gmail.com>
2024-03-19 15:56:54 -0400

    add godottpd addon and setup http server

--- Scripts/Websocket.gd
+++ Scripts/Websocket.gd
@@ -13,17 +13,31 @@ var players := {}
 # map of player ids to their scores
 var scores := {}
 
+# current num being used for name
+var curr_name = 1
+
 # let the other scripts know when a new player has joined
 signal client_connected(data)
 
 # called when a new player is discovered
 func register_player(player: Dictionary):
-	if !players.has(player.id) and is_instance_valid(players[player.id]):
-			# add this player to the list of current players
-			players[player.id] = player
-			
-			# set their score to zero
-			scores[player.id] = 0
+	if !players.has(player.id) and player.has("id"):
+		# check if this player sent a name
+		if !player.has("name"):
+			# add a number for their name
+			player["name"] = curr_name
+			curr_name += 1
+		
+		# add this player to the list of current players
+		players[player.id] = player
+		
+		# set their score to zero
+		scores[player.id] = 0
+		
+		# debug print
+		print("new player " + str(player.name) + " with id " + str(player.id))
+	else:
+		print("unrecognized new player packet")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,6 +46,9 @@ func _ready():
 		# prevents _process from being called if server doesn't start
 		print("error starting server")
 		set_process(false)
+		
+	# print out the server ip
+	print("running ws server at ws://" + IP.get_local_addresses()[1] + ":" + str(tcp.get_local_port()))
 
 # called from inside _process when receiving new packets
 func _recv(data):
@@ -45,7 +62,7 @@ func _recv(data):
 		var player = json.data
 		
 		# register a new player if they havent been seen yet
-		if !players.has(player.id) and is_instance_valid(players[player.id]):
+		if !players.has("id"):
 			# add this player to the list of current players
 			register_player(player)
 			
@@ -56,6 +73,9 @@ func _recv(data):
 			players[player.id].update_data(player, true)
 	else:
 		print("Unexpected packet: " + data)
+		
+		# print out json parsing errors
+		if err != OK: print("JSON parse error: " + json.get_error_message())
 
 # called each frame
 func _process(delta):
